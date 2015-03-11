d36a12057aff9ca7484b0f2b8117f91ebd6472c9
Jon Coppeard <jonco@pobox.com>
2015-03-10 22:44:54 -0400

    Add generator tests

---
+++ test/tests/generators.py
@@ -0,0 +1,31 @@
+# output: ok
+
+def foo(x):
+    yield 1
+    yield x
+
+iter = foo(2)
+assert iter.next() == 1
+assert iter.next() == 2
+
+def bar(array):
+    for x in array:
+        yield 2 * x
+
+iter = bar([1, 2, 3])
+assert iter.next() == 2
+assert iter.next() == 4
+assert iter.next() == 6
+
+def collect(iter):
+    result = []
+    for x in iter:
+        result.append(x)
+    return result
+
+r = collect(foo(0))
+assert len(r) == 2
+assert r[0] == 1
+assert r[1] == 0
+
+print('ok')
