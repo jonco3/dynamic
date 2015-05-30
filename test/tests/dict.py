a868a42da95b55de94f24a829331fe2aaa7bdd78
Jon Coppeard <jonco@pobox.com>
2015-05-30 15:54:03 +0100

    Add dict tests

---
+++ test/tests/dict.py
@@ -0,0 +1,38 @@
+# output: ok
+
+a = {}
+assert(len(a) == 0)
+
+for i in range(0, 100):
+    a[i] = i * 2
+
+assert(len(a) == 100)
+for i in range(0, 100):
+    assert i in a
+    assert(a[i] == i * 2)
+assert 101 not in a
+
+for i in range(0, 100, 2):
+    del a[i]
+assert(len(a) == 50)
+for i in range(0, 100):
+    # todo: False == 0 in python
+    assert (i in a) == not not (i % 2)
+
+a = {}
+for i in range(0, 100):
+    a[str(i)] = i
+assert(len(a) == 100)
+for i in range(0, 100):
+    k = str(i)
+    assert k in a
+    assert(a[k] == i)
+assert '101' not in a
+
+for i in range(0, 100, 2):
+    del a[str(i)]
+assert(len(a) == 50)
+for i in range(0, 100):
+    assert (str(i) in a) == not not (i % 2)
+
+print('ok')
