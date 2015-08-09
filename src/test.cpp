a8c6d49b1a4ac1cbd77fbb2b389ced350fdde84c
Jon Coppeard <jonco@pobox.com>
2015-08-09 14:24:17 +0100

    Split unittests main program from TestCase impl

--- src/test.cpp
+++ src/test.cpp
@@ -1,10 +1,5 @@
 #include "test.h"
 
-#include "common.h"
-#include "interp.h"
-
-#include "sysexits.h"
-
 #include <cassert>
 #include <cstdio>
 #include <cstring>
@@ -68,54 +63,4 @@ void maybeAbortTests(string what)
         cerr << "Exception thrown in test: " << what << endl;
         assert(false);
     }
-}
-
-const char* usageMessage =
-    "usage:\n"
-    "  tests              -- enter the unit tests\n"
-    "options:\n"
-    "  -l LIBDIR          -- set directory to load libraries from\n"
-#ifdef DEBUG
-    "  -le                -- log interpreter execution\n"
-    "  -lf                -- log interpreter frames only\n"
-    "  -lg                -- log GC activity\n"
-#endif
-    ;
-
-static void badUsage()
-{
-    cerr << usageMessage << endl;
-    exit(EX_USAGE);
-}
-
-int main(int argc, char *argv[])
-{
-    int pos = 1;
-
-    string libDir = ".";
-
-    while (pos != argc && argv[pos][0] == '-') {
-        const char* opt = argv[pos++];
-        if (strcmp("-l", opt) == 0 && pos != argc)
-            libDir = argv[pos++];
-#ifdef DEBUG
-        else if (strcmp("-le", opt) == 0)
-            logFrames = logExecution = true;
-        else if (strcmp("-lf", opt) == 0)
-            logFrames = true;
-        else if (strcmp("-lg", opt) == 0)
-            logGC = true;
-#endif
-        else
-            badUsage();
-    }
-
-    gc.minCollectAt = 10;
-    gc.scheduleFactorPercent = 110;
-
-    init1();
-    init2(libDir);
-    TestCase::runAllTests();
-    final();
-    return 0;
 }
