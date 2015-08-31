da48e072d17128603963f73dfde7c20de7d851fb
Jon Coppeard <jon@Jons-MBP.home>
2015-08-31 13:10:59 +0100

    Test augmented assigments fall back to standard operators

--- test/tests/augassign.py
+++ test/tests/augassign.py
@@ -75,4 +75,73 @@ v[11] |= 5
 
 assert list(map(lambda x: x.value, v)) == r
 
+class Wrapped2:
+    def __init__(self, initial):
+        self.value = initial
+    def __add__(self, other):
+        return Wrapped(self.value + other)
+    def __sub__(self, other):
+        return Wrapped(self.value - other)
+    def __mul__(self, other):
+        return Wrapped(self.value * other)
+    def __truediv__(self, other):
+        return Wrapped(self.value / other)
+    def __floordiv__(self, other):
+        return Wrapped(self.value // other)
+    def __mod__(self, other):
+        return Wrapped(self.value % other)
+    def __pow__(self, other):
+        return Wrapped(self.value ** other)
+    def __lshift__(self, other):
+        return Wrapped(self.value << other)
+    def __rshift__(self, other):
+        return Wrapped(self.value >> other)
+    def __or__(self, other):
+        return Wrapped(self.value | other)
+    def __and__(self, other):
+        return Wrapped(self.value & other)
+    def __xor__(self, other):
+        return Wrapped(self.value ^ other)
+    def __radd__(self, other):
+        return other + self.value
+    def __rsub__(self, other):
+        return other - self.value
+    def __rmul__(self, other):
+        return other * self.value
+    def __rtruediv__(self, other):
+        return other / self.value
+    def __rfloordiv__(self, other):
+        return other // self.value
+    def __rmod__(self, other):
+        return other % self.value
+    def __rpow__(self, other):
+        return other ** self.value
+    def __rlshift__(self, other):
+        return other << self.value
+    def __rrshift__(self, other):
+        return other >> self.value
+    def __ror__(self, other):
+        return other | self.value
+    def __rand__(self, other):
+        return other & self.value
+    def __rxor__(self, other):
+        return other ^ self.value
+
+v = list(map(Wrapped2, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]))
+
+v[0] += 1
+v[1] -= 2
+v[2] *= 3
+v[3] /= 4
+v[4] //= 5
+v[5] %= 6
+v[6] **= 7
+v[7] <<= 1
+v[8] >>= 2
+v[9] &= 3
+v[10] ^= 4
+v[11] |= 5
+
+assert list(map(lambda x: x.value, v)) == r
+
 print('ok')
