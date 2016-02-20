ba4eb2e56c7d7ced1fa8ac3cb955673927c84021
Jon Coppeard <jonco@pobox.com>
2016-02-20 10:34:40 +0000

    Add some more subscript tests

--- test/tests/slice.py
+++ test/tests/slice.py
@@ -1,9 +1,46 @@
 # output: ok
 
+class Listy:
+    def __init__(self, values):
+      self.values = values
+    def __getitem__(self, x):
+        return self.values[x]
+    def __setitem__(self, x, y):
+        self.values[x] = y
+
+# test subscript reference
+
+data = [0, 1, 2]
+assert data[0] == 0
+assert data[2] == 2
+assert data[-2] == 1
+
+data2 = Listy([1, 2, 3])
+assert data2[0] == 1
+assert data2[2] == 3
+assert data2[-2] == 2
+
+# test subscript assignment
+
+data = [0, 1, 2]
+data[0] = 3
+assert data == [3, 1, 2]
+data[-2] = 0
+assert data == [3, 0, 2]
+
+data2 = Listy([1, 2, 3])
+data2[0] = 4
+assert data2.values == [4, 2, 3]
+data2[-2] = 1
+assert data2.values == [4, 1, 3]
+
+# test subscript reference with slice
+
+data = [0, 1, 2]
+
 assert [][0:0] == []
 assert [][0:1] == []
 
-data = [0, 1, 2]
 assert data[0:0] == []
 assert data[0:1] == [0]
 assert data[0:2] == [0, 1]
